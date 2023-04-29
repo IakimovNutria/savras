39fcafb9d145ceb1671051e37fa50ca7a19ca269
Iakimov Sergey <serg.n.y@yandex.ru>
2023-04-29 18:03:13 +0500

    fix outputs

--- src/components/outputs-sidebar/outputs-sidebar.tsx
+++ src/components/outputs-sidebar/outputs-sidebar.tsx
@@ -11,32 +11,39 @@ type OutputsParams = {
 
 function OutputsSidebar({cellId}: OutputsParams): JSX.Element | null {
 	const dispatch = useAppDispatch();
-	const outputs = useAppSelector(getCurrentPipeline)?.cells.find((cell) => cell.id === cellId)?.outputs;
-	if (!outputs) {
+	const outputsPaths = useAppSelector(getCurrentPipeline)?.cells.find((cell) => cell.id === cellId)?.outputs;
+	if (!outputsPaths) {
 		return null;
 	}
-	const [localOutputs, setLocalOutputs] = useState(outputs);
+	const initialLocalOutputs = useMemo(() => {
+		const value: {[key: string]: string | null} = {};
+		for (const key in outputsPaths) {
+			value[key] = null;
+		}
+		return value;
+	}, []);
+	const [localOutputs, setLocalOutputs] = useState(initialLocalOutputs);
 	const saveFilesHandler = useCallback((event: FormEvent<HTMLButtonElement>) => {
 		event.preventDefault();
 		for (const key in localOutputs) {
 			const value = localOutputs[key];
 			if (value !== '' && value != null) {
-				const path = key;
+				const path = outputsPaths[key];
 				if (path !== null) {
 					dispatch(saveFile({ path, name: value }));
 				}
 			}
 		}
-	}, [dispatch, localOutputs]);
+	}, [dispatch, localOutputs, outputsPaths]);
 	const outputNames: string[] = useMemo(() => {
 		const newOutputNames = [];
-		for (const key in outputs) {
-			if (outputs[key] !== null) {
+		for (const key in outputsPaths) {
+			if (outputsPaths[key] !== null) {
 				newOutputNames.push(key);
 			}
 		}
 		return newOutputNames;
-	}, [outputs]);
+	}, [outputsPaths]);
 
 	return localOutputs && (
 		<Sidebar items={outputNames}
