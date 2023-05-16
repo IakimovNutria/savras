430b8b8bc325fc1773d3c3f3b31307f95168dfba
Iakimov Sergey <serg.n.y@yandex.ru>
2023-05-16 21:37:59 +0500

    fix files download

--- src/utils/download-file.ts
+++ src/utils/download-file.ts
@@ -3,6 +3,6 @@ import {createAPI} from '../services/api';
 
 export const downloadFile = (path: string, name: string) => {
 	const api = createAPI();
-	api.get('/files-section/download', { params: { path }, responseType: 'blob' })
+	api.get('/files/download', { params: { path }, responseType: 'blob' })
 		.then((r) => saveAs(r.data, name));
 };
