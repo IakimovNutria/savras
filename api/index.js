43d5eeda2db18b841863b4f22264d4af2ae9c103
IakimovNutria <serg.n.y@yandex.ru>
2023-06-04 21:21:40 +0500

    change back path

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 const app = express();
 
 app.use('/', createProxyMiddleware({
-	target: 'http://51.250.72.142:1337',
+	target: 'http://51.250.79.248:8000',
 	changeOrigin: true
 }));
 app.listen(3000);
